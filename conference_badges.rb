7c0f3fa489120464952c60f1eca35569da419198
Dural Keys <duralinitaly@gmail.com>
2020-02-18 16:55:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+
+def conference_badges(names)
+  hello my name is []
+  
+  {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+  conference_badges []
+  
+ end               
+
+hello my name is
+
+end 
+
+
+
+
+def badge-maker
+  
+end 
